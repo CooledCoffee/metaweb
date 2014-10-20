28e9b6e8794843d4dd187ec004b2700d72f5df65
CooledCoffee <cooledcoffee@gmail.com>
2014-10-20 10:06:04 +0800

    check for existance of web.py & webapp2

--- src/metaweb/impls/webapp2.py
+++ src/metaweb/impls/webapp2.py
@@ -1,33 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-from metaweb import coor, views
-from webapp2 import RequestHandler, WSGIApplication
+from decorated.util import modutil
 
-class GaeCoor(coor.coor_maker(RequestHandler)):
-    def get(self, path):
-        resp = self.render(path)
-        self._process_resp(resp)
-        
-    def post(self, path):
-        resp = self.render(path)
-        self._process_resp(resp)
-        
-    def _process_resp(self, resp):
-        self.response.set_status(resp.status())
-        for k, v in resp.headers.items():
-            self.response.headers[k] = v
-        self.response.out.write(resp.body)
-        
-    def _read_fields(self):
-        fields = dict(self.request.GET)
-        fields.update(self.request.POST)
-        return fields
+if modutil.module_exists('webapp2'):
+    from metaweb import coor, views
+    from webapp2 import RequestHandler, WSGIApplication
     
-    def _read_headers(self):
-        return self.request.headers
-    
-def start(default_url=None, roots=('views',)):
-    views.load(roots=roots)
-    if default_url is not None:
-        views.add_default_view(default_url)
-    return WSGIApplication([('(.*)', GaeCoor)], debug=False)
+    class GaeCoor(coor.coor_maker(RequestHandler)):
+        def get(self, path):
+            resp = self.render(path)
+            self._process_resp(resp)
+            
+        def post(self, path):
+            resp = self.render(path)
+            self._process_resp(resp)
+            
+        def _process_resp(self, resp):
+            self.response.set_status(resp.status())
+            for k, v in resp.headers.items():
+                self.response.headers[k] = v
+            self.response.out.write(resp.body)
+            
+        def _read_fields(self):
+            fields = dict(self.request.GET)
+            fields.update(self.request.POST)
+            return fields
+        
+        def _read_headers(self):
+            return self.request.headers
+        
+    def start(default_url=None, roots=('views',)):
+        views.load(roots=roots)
+        if default_url is not None:
+            views.add_default_view(default_url)
+        return WSGIApplication([('(.*)', GaeCoor)], debug=False)
--- src/metaweb/impls/webpy.py
+++ src/metaweb/impls/webpy.py
@@ -1,66 +1,69 @@
 # -*- coding: utf-8 -*-
-from cgi import FieldStorage
-from metaweb import files, coor, views
-from metaweb.files import FileField
-from web import webapi, utils
-from web.webapi import BadRequest
-import web
+from decorated.util import modutil
 
-class WebPyCoor(coor.coor_maker()):
-    def GET(self, path):
-        resp = self.render(path)
-        return self._process_resp(resp)
-
-    def POST(self, path):
-        resp = self.render(path)
-        return self._process_resp(resp)
+if modutil.module_exists('web'):
+    from cgi import FieldStorage
+    from metaweb import files, coor, views
+    from metaweb.files import FileField
+    from web import webapi, utils
+    from web.webapi import BadRequest
+    import web
     
-    def _read_fields(self):
-        fields = webapi.rawinput('both')
-        try:
-            defaults = {k: {} for k in fields}
-            fields = utils.storify(fields, _unicode=True, **defaults)
-            for k, v in fields.items():
-                if isinstance(v, FieldStorage):
-                    fields[k] = WebpyFileField(v)
-            return fields
-        except KeyError:
-            raise BadRequest()
+    class WebPyCoor(coor.coor_maker()):
+        def GET(self, path):
+            resp = self.render(path)
+            return self._process_resp(resp)
     
-    def _read_headers(self):
-        def _normalize_key(key):
-            key = key[len('HTTP_'):]
-            ss = key.split('_')
-            ss = [s.capitalize() for s in ss]
-            return '-'.join(ss)
-        return {_normalize_key(k): v \
-                for k, v in web.ctx.env.items() \
-                if k.startswith('HTTP_')}
-    
-    def _process_resp(self, resp):
-        web.ctx.status = resp.status()
-        for k, v in resp.headers.items():
-            web.header(k, v)
-        return resp.body
-    
-class WebpyFileField(FileField):
-    def __init__(self, storage):
-        super(WebpyFileField, self).__init__(storage.filename)
-        self._file = storage.file
+        def POST(self, path):
+            resp = self.render(path)
+            return self._process_resp(resp)
         
-    def chunks(self, size=files.CHUNK_SIZE):
-        while True:
-            chunk = self._file.read(size)
-            if len(chunk):
-                yield chunk
-            else:
-                break
+        def _read_fields(self):
+            fields = webapi.rawinput('both')
+            try:
+                defaults = {k: {} for k in fields}
+                fields = utils.storify(fields, _unicode=True, **defaults)
+                for k, v in fields.items():
+                    if isinstance(v, FieldStorage):
+                        fields[k] = WebpyFileField(v)
+                return fields
+            except KeyError:
+                raise BadRequest()
+        
+        def _read_headers(self):
+            def _normalize_key(key):
+                key = key[len('HTTP_'):]
+                ss = key.split('_')
+                ss = [s.capitalize() for s in ss]
+                return '-'.join(ss)
+            return {_normalize_key(k): v \
+                    for k, v in web.ctx.env.items() \
+                    if k.startswith('HTTP_')}
+        
+        def _process_resp(self, resp):
+            web.ctx.status = resp.status()
+            for k, v in resp.headers.items():
+                web.header(k, v)
+            return resp.body
+        
+    class WebpyFileField(FileField):
+        def __init__(self, storage):
+            super(WebpyFileField, self).__init__(storage.filename)
+            self._file = storage.file
             
-def start(cls=WebPyCoor, default_url=None, roots=('views',)):
-    views.load(roots=roots)
-    if default_url is not None:
-        views.add_default_view(default_url)
-    mapping = ('(.+)', cls)
-    app = web.application(mapping)
-    app.run()
-    
+        def chunks(self, size=files.CHUNK_SIZE):
+            while True:
+                chunk = self._file.read(size)
+                if len(chunk):
+                    yield chunk
+                else:
+                    break
+                
+    def start(cls=WebPyCoor, default_url=None, roots=('views',)):
+        views.load(roots=roots)
+        if default_url is not None:
+            views.add_default_view(default_url)
+        mapping = ('(.+)', cls)
+        app = web.application(mapping)
+        app.run()
+        
