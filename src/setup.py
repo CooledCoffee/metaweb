8bee5f88c82f34cadb26d8e40e1b50b2d510244d
CooledCoffee <cooledcoffee@gmail.com>
2014-12-16 15:10:23 +0800

    version 1.4

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='metaweb',
-    version='1.3',
+    version='1.4',
     author='Mengchen LEE',
     author_email='CooledCoffee@gmail.com',
     classifiers = [
