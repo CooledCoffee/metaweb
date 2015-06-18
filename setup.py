e796f1950af43e76b345265d5f8bd5060ad6fa46
CooledCoffee <cooledcoffee@gmail.com>
2015-06-18 17:15:12 +0800

    version 1.5.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name='metaweb',
-    version='1.5.4',
+    version='1.5.5',
     author='Mengchen LEE',
     author_email='CooledCoffee@gmail.com',
     classifiers = [
